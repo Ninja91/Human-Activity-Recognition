6c79fd95febdbea60f7ba6321ac9897a04f54f1c
Nitin Jain <jain117@purdue.edu>
2014-11-27 01:57:10 -0500

    Updating LDA, making it more modular. Also removing dataset directory as repository does not have enough space. Link to dataset will be posted in the Readme file

--- LDA.py
+++ LDA.py
@@ -4,14 +4,57 @@ import numpy as np
 from sklearn.lda import LDA
 import common
 
-XFull = common.parseFile('./UCI HAR Dataset/train/X_train.txt')
-YFull = common.parseFile('./UCI HAR Dataset/train/y_train.txt')
+def LDA_onFullDataset():
+    #Parsing Full training dataset
+    XFull = common.parseFile('../UCI HAR Dataset/train/X_train.txt')
+    YFull = common.parseFile('../UCI HAR Dataset/train/y_train.txt')
 
-XFullTest = common.parseFile('./UCI HAR Dataset/test/X_test.txt')
-YFullTest = common.parseFile('./UCI HAR Dataset/test/y_test.txt')
+    #Parsing Full testing dataset
+    XFullTest = common.parseFile('../UCI HAR Dataset/test/X_test.txt')
+    YFullTest = common.parseFile('../UCI HAR Dataset/test/y_test.txt')
 
-clf = LDA()
-clf.fit(XFull, YFull.flatten())
+    #Fitting data using LDA classifier
+    clf = LDA()
+    clf.fit(XFull, YFull.flatten())
 
-precision,recall,fscore = common.checkAccuracy(clf.predict(XFullTest),YFullTest,[1,2,3,4,5,6])
-print fscore
+    #Testing the results
+    precision,recall,fscore = common.checkAccuracy(clf.predict(XFullTest),YFullTest,[1,2,3,4,5,6])
+    print fscore
+
+def LDA_onNonDynamicData():
+    #Parsing Full training dataset
+    XFull = common.parseFile('../UCI HAR Dataset/train/X_train.txt')
+    YFull = common.parseFile('../UCI HAR Dataset/train/y_train.txt')
+
+    #Parsing Full testing dataset
+    XFullTest = common.parseFile('../UCI HAR Dataset/test/X_test.txt')
+    YFullTest = common.parseFile('../UCI HAR Dataset/test/y_test.txt')
+
+    #Getting the dataset associated with Non-Dynamic Activities on training 
+    X_NonDynamic,Y_NonDynamic = common.getDataSubset(XFull,YFull.flatten(),[4,5,6])
+    #Getting the dataset associated with Non-Dynamic Activities on testing
+    X_NonDynamicTest,Y_NonDynamicTest = common.getDataSubset(XFull,YFull.flatten(),[4,5,6])
+
+    #Fitting data using LDA classifier
+    clf = LDA()
+    clf.fit(X_NonDynamic, Y_NonDynamic.flatten())
+
+    precision,recall,fscore = common.checkAccuracy(clf.predict(X_NonDynamicTest),Y_NonDynamicTest,[4,5,6])
+    print fscore
+
+    #Getting the dataset associated with Dynamic Activities on training 
+    X_Dynamic,Y_Dynamic = common.getDataSubset(XFull,YFull.flatten(),[1,2,3])
+    #Getting the dataset associated with Dynamic Activities on testing
+    X_DynamicTest,Y_DynamicTest = common.getDataSubset(XFull,YFull.flatten(),[1,2,3])
+
+    #Fitting data using LDA classifier
+    clf = LDA()
+    clf.fit(X_Dynamic, Y_Dynamic.flatten())
+
+    precision,recall,fscore = common.checkAccuracy(clf.predict(X_DynamicTest),Y_DynamicTest,[1,2,3])
+
+    print fscore
+
+if __name__=='__main__':
+    LDA_onFullDataset()
+    LDA_onNonDynamicData()
