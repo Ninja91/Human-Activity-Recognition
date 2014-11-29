4cf80920089b3194ecf9f40474e30a9f88720ca1
Keerthi Raj <rajkn90@gmail.com>
2014-11-29 17:17:52 -0500

    Commented SVM.py

--- SVM.py
+++ SVM.py
@@ -15,48 +15,41 @@ from matplotlib import pyplot as plt
 # Prints the numbers in float instead of scientific format
 set_printoptions(suppress=True)
 
-filename='../UCI HAR Dataset/' # Dataset Used
+filename='../UCI HAR Dataset/' 							# Dataset Used (Change as per your computer's path)
 #--------------------------------------------------------------------------------------------#
 
-#---------------------------------------------------------------------------------------------#
+X_train=common.parseFile(filename+'train/X_train.txt')				# Read X Train 
+Y_train=(common.parseFile(filename+'train/y_train.txt')).flatten()		# Read Y Train and flatten it to 1D array
+X_test=common.parseFile(filename+'test/X_test.txt')				# Read X Test
+Y_test=(common.parseFile(filename+'test/y_test.txt')).flatten()			# Read Y test and flatten it to 1D array
 
-X_train=common.parseFile(filename+'train/X_train.txt')
-Y_train=(common.parseFile(filename+'train/y_train.txt')).flatten()
-X_test=common.parseFile(filename+'test/X_test.txt')
-Y_test=(common.parseFile(filename+'test/y_test.txt')).flatten()
-#Y_train=common.convertLabel(Y_train)
-#Y_test=common.convertLabel(Y_test)
-print len(X_train), len(Y_train)
-print len(X_test), len(Y_test), 
-#print X_test.flags
+print len(X_train), len(Y_train)						# Printing Lengths of Train and Test Data
+print len(X_test), len(Y_test)
 
-X_dynamic_train, Y_dynamic_train=common.getDataSubset(X_train, Y_train, [1,2,3])
-X_nondynamic_train, Y_nondynamic_train=common.getDataSubset(X_train, Y_train, [1,2,3])
+X_dynamic_train, Y_dynamic_train=common.getDataSubset(X_train, Y_train, [1,2,3])	# Get Train sub data for [1,2,3]
+X_nondynamic_train, Y_nondynamic_train=common.getDataSubset(X_train, Y_train, [4,5,6])  # Get Train sub data for [4,5,6]
 
-X_dynamic_test, Y_dynamic_test=common.getDataSubset(X_test, Y_test, [1,2,3])
-X_nondynamic_test, Y_nondynamic_test=common.getDataSubset(X_test, Y_test, [1,2,3])
+X_dynamic_test, Y_dynamic_test=common.getDataSubset(X_test, Y_test, [1,2,3])		# Get Test sub data for [1,2,3]
+X_nondynamic_test, Y_nondynamic_test=common.getDataSubset(X_test, Y_test, [4,5,6])	# Get Test sub data for [4,5,6]
 
-X_nondynamic_train=common.getPowerK(X_nondynamic_train, [1,2])
-X_nondynamic_test=common.getPowerK(X_nondynamic_test, [1,2])
-#X_nondynamic_train_6, Y_nondynamic_train_6=common.getDataSubset(X_train, Y_train, [6])
+X_nondynamic_train=common.getPowerK(X_nondynamic_train, [1,2])				# Convert X Train to X+X^2
+X_nondynamic_test=common.getPowerK(X_nondynamic_test, [1,2])				# Convert X Test to X+X^2
+#X_nondynamic_train_6, Y_nondynamic_train_6=common.getDataSubset(X_train, Y_train, [6]) # Used earlier to get Sub data for just 6th label
 #X_nondynamic_test_6, Y_nondynamic_test_6=common.getDataSubset(X_test, Y_test, [6])
-#Y_nondynamic_train_sublabels=common.convertLabel(Y_nondynamic_train, [4,5], [6])
+#Y_nondynamic_train_sublabels=common.convertLabel(Y_nondynamic_train, [4,5], [6])	# Used earlier to convert [4,5] to [1] and [6] to [0]
 #Y_nondynamic_test_sublabels=common.convertLabel(Y_nondynamic_test, [4,5], [6])
 
-print len(X_dynamic_train), len(Y_dynamic_train), Y_dynamic_train
+print len(X_dynamic_train), len(Y_dynamic_train), Y_dynamic_train		# Printing lenghts and Labels extracted for verification
 print len(X_nondynamic_train), len(Y_nondynamic_train), Y_nondynamic_train
 
-sample_weights=common.getSampleWeights( X_nondynamic_train, Y_nondynamic_train , [1,2,3])
-print sample_weights
-#class_weights=dict()
-#class_weights[1]=1.0
-#class_weights[2]=1.0
-#class_weights[3]=1.0
+sample_weights=common.getSampleWeights( X_nondynamic_train, Y_nondynamic_train , [4,5,6])	# Get sample weights for non-dynamic Data
+#print sample_weights
+
+################################################################################################
+#Code used for Dynamic Data - Commented for now
+
 '''clf = svm.LinearSVC(multi_class='crammer_singer')
 clf.fit(X_dynamic_train, Y_dynamic_train)
-
-#clf = neighbors.KNeighborsClassifier(50, weights='distance')
-#clf.fit(X_dynamic_train, Y_dynamic_train)
 Y_predict_dynamic=clf.predict(X_dynamic_test)
 print type(Y_predict_dynamic), size(Y_predict_dynamic), Y_predict_dynamic
 prec, rec, f_score=common.checkAccuracy(Y_dynamic_test, Y_predict_dynamic, [1,2,3])
@@ -65,24 +58,15 @@ print rec
 print f_score
 print common.createConfusionMatrix(Y_predict_dynamic, Y_dynamic_test, [1,2,3])
 #print clf.n_support_'''
+################################################################################################
 
-#clf = neighbors.KNeighborsClassifier(1, weights='distance')
-#clf.fit(X_nondynamic_train, Y_nondynamic_train)
-#class_weights=dict()
-#class_weights[4]=2.5
-#class_weights[5]=1.0
+# SVM Code for Linear Kernel with sample weights for non-dynamic classes [4,5,6]
 clf = svm.SVC(kernel='linear')
-clf.fit(X_nondynamic_train, Y_nondynamic_train, sample_weight=sample_weights)
-Y_predict_nondynamic=clf.predict(X_nondynamic_test)
-print type(Y_predict_nondynamic), size(Y_predict_nondynamic), Y_predict_nondynamic
-prec, rec, f_score=common.checkAccuracy(Y_nondynamic_test, Y_predict_nondynamic, [1,2,3])
-print prec
+clf.fit(X_nondynamic_train, Y_nondynamic_train, sample_weight=sample_weights) 		# Fit SVM using sample weights
+Y_predict_nondynamic=clf.predict(X_nondynamic_test)					# Predict Labels for test data
+print type(Y_predict_nondynamic), size(Y_predict_nondynamic), Y_predict_nondynamic	# Print Lenghts and predicted labels for verification
+prec, rec, f_score=common.checkAccuracy(Y_nondynamic_test, Y_predict_nondynamic, [4,5,6]) # Check accuracy
+print prec										# Print Precision, Recall and f-score
 print rec
 print f_score
-print common.createConfusionMatrix(Y_predict_nondynamic, Y_nondynamic_test, [1,2,3])
-#print clf.n_support_
-
-#SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0, degree=3,
-#gamma=0.0, kernel='rbf', max_iter=-1, probability=False, random_state=None,
-#shrinking=True, tol=0.001, verbose=False)
-#clf.predict([[2., 2.]])
+print common.createConfusionMatrix(Y_predict_nondynamic, Y_nondynamic_test, [4,5,6])	# Print Confusion Matrix for the same
