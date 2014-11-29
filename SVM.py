b5fbd3785b1af6bddb8620b5628135a9770df263
Keerthi Raj <rajkn90@gmail.com>
2014-11-28 21:38:42 -0500

    Updated SVM.py

--- SVM.py
+++ SVM.py
@@ -31,17 +31,23 @@ print len(X_test), len(Y_test),
 #print X_test.flags
 
 X_dynamic_train, Y_dynamic_train=common.getDataSubset(X_train, Y_train, [1,2,3])
-X_nondynamic_train, Y_nondynamic_train=common.getDataSubset(X_train, Y_train, [4,5])
+X_nondynamic_train, Y_nondynamic_train=common.getDataSubset(X_train, Y_train, [1,2,3])
 
 X_dynamic_test, Y_dynamic_test=common.getDataSubset(X_test, Y_test, [1,2,3])
-X_nondynamic_test, Y_nondynamic_test=common.getDataSubset(X_test, Y_test, [4,5])
+X_nondynamic_test, Y_nondynamic_test=common.getDataSubset(X_test, Y_test, [1,2,3])
 
-X_nondynamic_train_6, Y_nondynamic_train_6=common.getDataSubset(X_train, Y_train, [6])
-X_nondynamic_test_6, Y_nondynamic_test_6=common.getDataSubset(X_test, Y_test, [6])
+X_nondynamic_train=common.getPowerK(X_nondynamic_train, [1,2])
+X_nondynamic_test=common.getPowerK(X_nondynamic_test, [1,2])
+#X_nondynamic_train_6, Y_nondynamic_train_6=common.getDataSubset(X_train, Y_train, [6])
+#X_nondynamic_test_6, Y_nondynamic_test_6=common.getDataSubset(X_test, Y_test, [6])
+#Y_nondynamic_train_sublabels=common.convertLabel(Y_nondynamic_train, [4,5], [6])
+#Y_nondynamic_test_sublabels=common.convertLabel(Y_nondynamic_test, [4,5], [6])
 
 print len(X_dynamic_train), len(Y_dynamic_train), Y_dynamic_train
 print len(X_nondynamic_train), len(Y_nondynamic_train), Y_nondynamic_train
 
+sample_weights=common.getSampleWeights( X_nondynamic_train, Y_nondynamic_train , [1,2,3])
+print sample_weights
 #class_weights=dict()
 #class_weights[1]=1.0
 #class_weights[2]=1.0
@@ -62,20 +68,19 @@ print common.createConfusionMatrix(Y_predict_dynamic, Y_dynamic_test, [1,2,3])
 
 #clf = neighbors.KNeighborsClassifier(1, weights='distance')
 #clf.fit(X_nondynamic_train, Y_nondynamic_train)
-class_weights=dict()
-class_weights[4]=2.5
-class_weights[5]=1.0
-clf = svm.LinearSVC(multi_class='crammer_singer', class_weight=class_weights)
-clf.fit(X_nondynamic_train, Y_nondynamic_train)
+#class_weights=dict()
+#class_weights[4]=2.5
+#class_weights[5]=1.0
+clf = svm.SVC(kernel='linear')
+clf.fit(X_nondynamic_train, Y_nondynamic_train, sample_weight=sample_weights)
 Y_predict_nondynamic=clf.predict(X_nondynamic_test)
 print type(Y_predict_nondynamic), size(Y_predict_nondynamic), Y_predict_nondynamic
-prec, rec, f_score=common.checkAccuracy(Y_nondynamic_test, Y_predict_nondynamic, [4,5])
+prec, rec, f_score=common.checkAccuracy(Y_nondynamic_test, Y_predict_nondynamic, [1,2,3])
 print prec
 print rec
 print f_score
-print common.createConfusionMatrix(Y_predict_nondynamic, Y_nondynamic_test, [4,5])
+print common.createConfusionMatrix(Y_predict_nondynamic, Y_nondynamic_test, [1,2,3])
 #print clf.n_support_
-
 
 #SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0, degree=3,
 #gamma=0.0, kernel='rbf', max_iter=-1, probability=False, random_state=None,
